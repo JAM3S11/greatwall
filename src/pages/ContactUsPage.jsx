0c3056919cfb19bfdc2762056067bd9a1ab93362
JAM3S11 <jdndirangu2020@gmail.com>
2025-12-28 19:00:55 +0300

    Update commit

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
-import { Mail, Phone, MapPin, Send } from 'lucide-react';
+import React, { useState } from 'react';
+import { Mail, Phone, MapPin, Send, ChevronDown } from 'lucide-react';
+import { Listbox, ListboxButton, ListboxOptions, ListboxOption, Transition } from '@headlessui/react';
+
+const querys = ["General Inquiry", "Technical Support", "Parternship"];
+
+const SelectPane = ({ value, onChange, options }) => (
+  <Listbox value={value} onChange={onChange}>
+    <div className='relative mt-1'>
+      <ListboxButton className="relative w-full cursor-pointer rounded-lg py-3 pl-4 pr-10 bg-slate-50 border border-slate-200 hover:border-primary/50 transition-colors focus:outline-none">
+        <span className='block truncate text-slate-400 font-normal text-left'>
+          {value ? value : "Select query"}
+        </span>
+        <span className='absolute inset-y-0 right-0 flex items-center pr-2'>
+          <ChevronDown className='w-4 h-4 text-gray-400' />
+        </span>
+      </ListboxButton>
+      <Transition leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+        <ListboxOptions className="absolute z-50 mt-1 max-h-40 w-full overflow-auto rounded-lg bg-white text-base shadow-2xl ring-1 ring-blue-300 ring-opacity-5 focus:outline-none sm:text-sm">
+          {options.map((queue, i) => (
+            <ListboxOption 
+              key={i}
+              className={({ active }) => `relative cursor-default select-none py-3 pl-10 pr-4 ${active ? 'bg-blue-50 text-blue-500' : 'text-gray-900'}`}
+              value={queue}
+            >
+              <span className='block, truncate'>{queue}</span>
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Transition>
+    </div>
+  </Listbox>
+)
 
 const ContactUsPage = () => {
+  const [selectedQuery, setSelectedQuery] = useState(querys[0]);
+
   return (
     <div className="bg-white text-slate-900 min-h-screen relative overflow-hidden">
       
@@ -74,11 +107,12 @@ const ContactUsPage = () => {
                   <input type="text" placeholder="Company" className="w-full bg-slate-50 border border-slate-200 p-4 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#135bec]/20 focus:border-[#135bec] transition-all" />
                 </div>
                 <input type="email" placeholder="Email Address" className="w-full bg-slate-50 border border-slate-200 p-4 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#135bec]/20 focus:border-[#135bec] transition-all" />
-                <select className="w-full bg-slate-50 border border-slate-200 p-4 rounded-xl focus:outline-none text-slate-500">
+                {/* <select className="w-full bg-slate-50 border border-slate-200 p-4 rounded-xl focus:outline-none text-slate-500">
                   <option>General Inquiry</option>
                   <option>Technical Support</option>
                   <option>Partnership</option>
-                </select>
+                </select> */}
+                <SelectPane value={selectedQuery} onChange={setSelectedQuery} options={querys} />
                 <textarea placeholder="How can we help you scale your energy infrastructure?" rows="4" className="w-full bg-slate-50 border border-slate-200 p-4 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#135bec]/20 focus:border-[#135bec] transition-all"></textarea>
                 <button className="w-full bg-[#135bec] text-white py-4 rounded-xl font-bold hover:bg-blue-700 transition-all flex items-center justify-center gap-2 shadow-lg shadow-blue-500/20 active:scale-95">
                   Send Message <Send size={18} />
